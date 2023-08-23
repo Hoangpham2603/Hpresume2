81ce1c76bdb29710eb233af4043e9cd81b320dad
Hoangpham2603 <kunpham2603@gmail.com>
2023-08-23 11:33:40 -0700

    About Done

--- components/TopLeftImg.jsx
+++ components/TopLeftImg.jsx
@@ -3,7 +3,13 @@ import Image from "next/image";
 const TopLeftImg = () => {
   return (
     <div className="absolute top-0 left-0 mix-blend-color-dodge w-[200px] z-10 xl:w-[400px] opacity-50">
-      <Image src="/top-left-img.png" width={400} height={300} alt="" />
+      <Image
+        src="/top-left-img.png"
+        width={400}
+        height={300}
+        alt=""
+        priority={true}
+      />
     </div>
   );
 };
--- pages/about/index.jsx
+++ pages/about/index.jsx
@@ -25,6 +25,8 @@ import Circles from "../../components/Circles";
 import { motion } from "framer-motion";
 import { fadeIn } from "../../variants";
 
+//counter
+import CountUp from "react-countup";
 //  data
 const aboutData = [
   {
@@ -114,19 +116,125 @@ const About = () => {
         <Avatar />
       </motion.div>
       <div className="container mx-auto h-full w-full flex flex-col items-center xl:flex-row gap-x-6">
-        <div>text</div>
-        <div className="flex gap-x-4 xl:gap-x-8 mx-auto">
-          {aboutData.map((item, index) => {
-            return (
-              <div
-                className="cursor-pointer capitalize xl:text-lg relative after:w-8 after:h-[2px] after:bg-white after:absolute after:-bottom-1 after:left-0"
-                key={index}
-              >
-                {item.title}
+        {/* text */}
+        <div className=" flex-1 flex flex-col justify-center">
+          <motion.h2
+            variants={fadeIn("right", 0.6)}
+            initial="hidden"
+            animate="show"
+            exit="hidden"
+            className="h2"
+          >
+            Captivating <span className="text-accent/80">stories</span> birth
+            magnificent designs.
+          </motion.h2>
+          <motion.p
+            variants={fadeIn("right", 0.5)}
+            initial="hidden"
+            animate="show"
+            exit="hidden"
+            className="max-w-[500px] mx-auto xl:mx-0 mb-6 xl:mb-12 px-2 xl:px-0"
+          >
+            {" "}
+            4 years ago a develop a thing call depression and i still have it to
+            this very day xD
+          </motion.p>
+
+          {/* counter */}
+          <motion.div
+            variants={fadeIn("right", 0.4)}
+            initial="hidden"
+            animate="show"
+            exit="hidden"
+            className="hidden md:flex md:max-w-xl xl:max-w-none mx-auto xl:mx-0"
+          >
+            <div className="flex flex-1 xl:gap-x-6 justify-center items-center">
+              {/* Experience */}
+              <div className="relative flex-1 after:w-[1px] after:h-full after:bg-white/10 after:absolute after:top-0 after:right-0">
+                <div className="text-2xl xl:text-4xl font-extrabold text-accent/80 mb-2">
+                  <CountUp start={0} end={10} duration={5} /> +
+                </div>
+                <div className="text-xs uppercase tracking-[1px] leading-[1.4] max-w-[100px] text-center md:text-center">
+                  Years of nothing
+                </div>
               </div>
-            );
-          })}
+
+              {/* clients */}
+              <div className="relative flex-1 after:w-[1px] after:h-full after:bg-white/10 after:absolute after:top-0 after:right-0">
+                <div className="text-2xl xl:text-4xl font-extrabold text-accent/80 mb-2">
+                  <CountUp start={0} end={250} duration={5} /> +
+                </div>
+                <div className="text-xs uppercase tracking-[1px] leading-[1.4] max-w-[100px] text-center md:text-center sm:text-center">
+                  Sastisfied womans
+                </div>
+              </div>
+
+              {/* Projects */}
+              <div className="relative flex-1 after:w-[1px] ">
+                <div className="text-2xl xl:text-4xl font-extrabold text-accent/80 mb-2">
+                  <CountUp start={0} end={650} duration={5} /> +
+                </div>
+                <div className="text-xs uppercase tracking-[1px] leading-[1.4] max-w-[100px] text-center md:text-center">
+                  Sastisfied womans
+                </div>
+              </div>
+            </div>
+          </motion.div>
         </div>
+
+        {/* info */}
+        <motion.div
+          variants={fadeIn("right", 0.3)}
+          initial="hidden"
+          animate="show"
+          exit="hidden"
+          className="flex flex-col w-full xl:max-w-[48%] h-[480px] mt-10"
+        >
+          <div className="flex gap-x-4 xl:gap-x-8 mx-auto mb-5">
+            {aboutData.map((item, itemIndex) => {
+              return (
+                <div
+                  onClick={() => {
+                    setIndex(itemIndex);
+                  }}
+                  className={` ${
+                    index === itemIndex &&
+                    "text-accent/80 after:w-[100%] after:bg-accent after:transition-all after:duration-300"
+                  } cursor-pointer capitalize xl:text-lg relative after:w-8 after:h-[2px] after:bg-white after:absolute after:-bottom-1 after:left-0`}
+                  key={itemIndex}
+                >
+                  {item.title}
+                </div>
+              );
+            })}
+          </div>
+          <div className="py-2 xl:py-6 flex flex-col gap-y-2 xl:gap-y-4 items-center xl:items-start mt-5">
+            {aboutData[index].info.map((item, itemIndex) => {
+              return (
+                <div
+                  key={itemIndex}
+                  className="flex-1 flex flex-col md:flex-row max-w-max gap-x-2 items-center text-wjote/60"
+                >
+                  {/* Title */}
+                  <div className="font-light mb-2 md:mb-0">{item.title}</div>
+                  <div className="hidden md:flex">-</div>
+                  <div>{item.stage}</div>
+
+                  <div className="flex gap-x-4">
+                    {/* icon */}
+                    {item.icons?.map((icon, itemIndex) => {
+                      return (
+                        <div key={itemIndex} className="text-2xl">
+                          {icon}
+                        </div>
+                      );
+                    })}
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        </motion.div>
       </div>
     </div>
   );
