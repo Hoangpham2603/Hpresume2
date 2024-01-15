eea80a952cc2c7f947ee2b61e9c88d94365b7eb4
hoangpham2603 <kunpham2603@gmail.com>
2024-01-14 22:43:38 -0800

    fixing nav

--- components/Header.jsx
+++ components/Header.jsx
@@ -8,7 +8,7 @@ const Header = () => {
   return (
     <header className="absolute z-30 w-full flex items-center px-16 xl:px-0 xl:h-[90px]">
       <div className="container mx-auto">
-        <div className="flex flex-col lg:flex-row justify-between items-center mt-3 gap-y-6">
+        <div className="flex flex-col lg:flex-row justify-between items-center mt-3 gap-y-2">
           {/* Logo */}
           <Link href={"/"}>
             {/* <Image
